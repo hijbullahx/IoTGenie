11861f6d30d466212209b7d733c79085da6ecedd
hijbullahx <hijbullah119445@gmail.com>
2025-06-16 20:47:39 +0600

    Prepared IoTGenie for Render deployment

--- IoTGenie/wsgi.py
+++ IoTGenie/wsgi.py
@@ -1,16 +1,5 @@
-"""
-WSGI config for IoTGenie project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "IoTGenie.settings")
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IoTGenie.settings')
 application = get_wsgi_application()
